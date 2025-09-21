497d244783817cff9ecc3ea5bd2c2c0b6a7d5246
trentashford27 <trentashford@oru.edu>
2025-09-21 16:22:22 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -6,20 +6,24 @@ let experience = "Action Fire Protection, Kiefer Fire Department, and volunteer 
 let skills = ["effective communication", "critical thinking", "weightlifting"];
 let skillsList = document.createElement("ul");
 
+
+// Connect variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("education").textContent = education;
+document.getElementById("experience").textContent = experience;
+
 for (let i = 0; i < skills.length; i++) {
  let li = document.createElement("li");
  li.textContent = skills[i];
  skillsList.appendChild(li);
 }
 
-document.body.appendChild(skillsList);
+document.getElementById("skills").insertBefore(
+  skillsList,
+  document.querySelector("#skills button")
+);
 
 function showFunFact() {
  alert("Fun Fact: I love football and baseball!");
 }
-
-// Connect variables to the HTML (ligaments)
-document.getElementById("my-name").textContent = myName;
-document.getElementById("title").textContent = title;
-document.getElementById("education").textContent = "Education: " + education;
-document.getElementById("experience").textContent = "Experience: " + experience;
